e6e82188dc04a58edcc1cfb0e8e629e80d248574
Arno Puder <arno@puder.org>
2013-03-23 10:30:50 -0700

    Use FrameLayout for correct compositing of the SurfaceView

--- gen/org/puder/trs80/R.java
+++ gen/org/puder/trs80/R.java
@@ -105,10 +105,10 @@ containing a value of this type.
         public static final int key_Y=0x7f070019;
         public static final int key_Z=0x7f07002f;
         public static final int keyboard=0x7f070005;
-        public static final int keyboard_container=0x7f070001;
+        public static final int keyboard_container=0x7f070002;
         public static final int menu_settings=0x7f07003b;
         public static final int path=0x7f070003;
-        public static final int screen=0x7f070002;
+        public static final int screen=0x7f070001;
     }
     public static final class layout {
         public static final int emulator=0x7f030000;
