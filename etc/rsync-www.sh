27eaf6b9ed5ce2558cd416be0cc1dfb5e07663b4
Arno Puder <arno@puder.org>
2014-11-14 16:26:32 -0800

    Script to upload receiver app to Google Drive

--- etc/rsync-www.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-if [ -d var/googlecast-receiver ]; then
-  cd var/googlecast-receiver
-  rsync -aiv ./ apuder,trs80@web.sourceforge.net:/home/project-web/trs80/htdocs/
-fi
-
---
+++ etc/upload-receiver-app.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+if [ -d var/googlecast-receiver ]; then
+  cp -r var/googlecast-receiver ~/Google\ Drive/TRS80/
+fi
+
