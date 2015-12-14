113b67ddd7a7b758e21b4b80ae2512fa56ffe4bb
Christopher J Kriese <ckriese@mybasis.com>
2015-12-14 12:42:58 -0800

    Add ORM model classes

---
+++ app/src/main/java/org/puder/trs80/market/MarketDatabase.java
@@ -0,0 +1,12 @@
+package org.puder.trs80.market;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+@Database(name = MarketDatabase.NAME, version = MarketDatabase.VERSION)
+
+public class MarketDatabase {
+
+    public static final String NAME = "Market";
+    public static final int VERSION = 1;
+
+}
