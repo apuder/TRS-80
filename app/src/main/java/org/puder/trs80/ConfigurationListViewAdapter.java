apuder/TRS-80

commit dedb8d25532fd9b89760b70d1350019379a6ed17

Remove DragSortRecycler.java

app/src/main/java/org/puder/trs80/ConfigurationListViewAdapter.java
@@ -18,17 +18,36 @@ package org.puder.trs80;
 
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.puder.trs80.drag.ItemTouchHelperAdapter;
+import org.puder.trs80.drag.OnStartDragListener;
+
 public class ConfigurationListViewAdapter extends
-        RecyclerView.Adapter<ConfigurationListViewAdapter.Holder> {
+        RecyclerView.Adapter<ConfigurationListViewAdapter.Holder> implements ItemTouchHelperAdapter {
 
     private ConfigurationMenuListener listener;
+    private final OnStartDragListener mDragStartListener;
+
+    @Override
+    public boolean onItemMove(int fromPosition, int toPosition) {
+        Configuration.move(fromPosition - 1, fromPosition - 1);
+        notifyItemMoved(fromPosition, toPosition);
+        return true;
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+
+    }
 
 
     class Holder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -41,6 +60,7 @@ public class ConfigurationListViewAdapter extends
         public TextView       sound;
         public TextView       keyboards;
         public ScreenshotView screenshot;
+        public ImageView      reorderHandle;
 
         private View          menu;
 
@@ -57,6 +77,7 @@ public class ConfigurationListViewAdapter extends
             sound = (TextView) itemView.findViewById(R.id.configuration_sound);
             keyboards = (TextView) itemView.findViewById(R.id.configuration_keyboards);
             screenshot = (ScreenshotView) itemView.findViewById(R.id.configuration_screenshot);
+            reorderHandle = (ImageView) itemView.findViewById(R.id.configuration_reorder);
             menu = itemView.findViewById(R.id.configuration_menu);
             menu.setOnClickListener(this);
             itemView.setOnClickListener(this);
@@ -76,8 +97,9 @@ public class ConfigurationListViewAdapter extends
     }
 
 
-    public ConfigurationListViewAdapter(ConfigurationMenuListener listener) {
+    public ConfigurationListViewAdapter(ConfigurationMenuListener listener, OnStartDragListener dragListener) {
         this.listener = listener;
+        this.mDragStartListener = dragListener;
     }
 
     @Override
@@ -92,6 +114,16 @@ public class ConfigurationListViewAdapter extends
             return;
         }
         Configuration conf = Configuration.getNthConfiguration(position - 1);
+
+        holder.reorderHandle.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                    mDragStartListener.onStartDrag(holder);
+                }
+                return false;
+            }
+        });
 
         // Position
         holder.position = position;

.idea/gradle.xml
@@ -3,8 +3,9 @@
   <component name="GradleSettings">
     <option name="linkedExternalProjectsSettings">
       <GradleProjectSettings>
-        <option name="distributionType" value="DEFAULT_WRAPPED" />
+        <option name="distributionType" value="LOCAL" />
         <option name="externalProjectPath" value="$PROJECT_DIR$" />
+        <option name="gradleHome" value="$APPLICATION_HOME_DIR$/gradle/gradle-2.10" />
         <option name="gradleJvm" value="1.8" />
         <option name="modules">
           <set>

app/app.iml
@@ -64,14 +64,6 @@
       <sourceFolder url="file://$MODULE_DIR$/src/main/java" isTestSource="false" />
       <sourceFolder url="file://$MODULE_DIR$/src/main/rs" isTestSource="false" />
       <sourceFolder url="file://$MODULE_DIR$/src/main/shaders" isTestSource="false" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/res" type="java-test-resource" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/resources" type="java-test-resource" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/assets" type="java-test-resource" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/aidl" isTestSource="true" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/java" isTestSource="true" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/jni" isTestSource="true" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/rs" isTestSource="true" />
-      <sourceFolder url="file://$MODULE_DIR$/src/test/shaders" isTestSource="true" />
       <sourceFolder url="file://$MODULE_DIR$/src/androidTest/res" type="java-test-resource" />
       <sourceFolder url="file://$MODULE_DIR$/src/androidTest/resources" type="java-test-resource" />
       <sourceFolder url="file://$MODULE_DIR$/src/androidTest/assets" type="java-test-resource" />
@@ -80,6 +72,14 @@
       <sourceFolder url="file://$MODULE_DIR$/src/androidTest/jni" isTestSource="true" />
       <sourceFolder url="file://$MODULE_DIR$/src/androidTest/rs" isTestSource="true" />
       <sourceFolder url="file://$MODULE_DIR$/src/androidTest/shaders" isTestSource="true" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/res" type="java-test-resource" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/resources" type="java-test-resource" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/assets" type="java-test-resource" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/aidl" isTestSource="true" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/java" isTestSource="true" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/jni" isTestSource="true" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/rs" isTestSource="true" />
+      <sourceFolder url="file://$MODULE_DIR$/src/test/shaders" isTestSource="true" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/assets" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/blame" />
       <excludeFolder url="file://$MODULE_DIR$/build/intermediates/builds" />
@@ -139,12 +139,12 @@
     <orderEntry type="library" exported="" name="commons-io-2.4" level="project" />
     <orderEntry type="library" exported="" name="jsonschema2pojo-core-0.4.23" level="project" />
     <orderEntry type="library" exported="" name="animated-vector-drawable-23.2.1" level="project" />
-    <orderEntry type="library" exported="" name="jsonschema2pojo-gradle-plugin-0.4.23" level="project" />
     <orderEntry type="library" exported="" name="jackson-databind-2.2.0" level="project" />
+    <orderEntry type="library" exported="" name="jsonschema2pojo-gradle-plugin-0.4.23" level="project" />
     <orderEntry type="library" exported="" name="annotations-1.3.9" level="project" />
     <orderEntry type="library" exported="" name="recyclerview-v7-23.2.1" level="project" />
-    <orderEntry type="library" exported="" name="jackson-core-asl-1.9.11" level="project" />
     <orderEntry type="library" exported="" name="support-annotations-23.2.1" level="project" />
+    <orderEntry type="library" exported="" name="jackson-core-asl-1.9.11" level="project" />
     <orderEntry type="library" exported="" name="jackson-core-2.2.0" level="project" />
     <orderEntry type="library" exported="" name="palette-v7-23.2.1" level="project" />
     <orderEntry type="library" exported="" name="support-vector-drawable-23.2.1" level="project" />
@@ -164,8 +164,8 @@
     <orderEntry type="library" exported="" name="beta-1.1.3" level="project" />
     <orderEntry type="library" exported="" name="simple-transport-6.0.1" level="project" />
     <orderEntry type="library" exported="" name="crashlytics-ndk-1.1.2" level="project" />
-    <orderEntry type="library" exported="" name="commons-lang3-3.2.1" level="project" />
     <orderEntry type="library" exported="" name="jackson-mapper-asl-1.9.11" level="project" />
+    <orderEntry type="library" exported="" name="commons-lang3-3.2.1" level="project" />
     <orderEntry type="library" exported="" name="play-services-tasks-9.2.1" level="project" />
     <orderEntry type="library" exported="" name="gson-2.5" level="project" />
     <orderEntry type="module" module-name="tpk" exported="" />

app/src/main/java/com/emtronics/dragsortrecycler/DragSortRecycler.java
@@ -1,497 +1,0 @@
-/*
- * DragSortRecycler
- *
- * Added drag and drop functionality to your RecyclerView
- *
- *
- * Copyright 2014 Emile Belanger.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-// https://github.com/emileb/DragSortRecycler
-
-package com.emtronics.dragsortrecycler;
-
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import java.lang.reflect.Modifier;
-
-
-public class DragSortRecycler extends RecyclerView.ItemDecoration implements RecyclerView.OnItemTouchListener {
-
-    final String TAG = "DragSortRecycler";
-
-    final boolean DEBUG = false;
-
-    private int dragHandleWidth = 0;
-
-    private int selectedDragItemPos = -1;
-
-    private int fingerAnchorY;
-
-    private int fingerY;
-
-    private int fingerOffsetInViewY;
-
-    private float autoScrollWindow = 0.1f;
-    private float autoScrollSpeed  = 0.5f;
-
-    private BitmapDrawable floatingItem;
-    private Rect           floatingItemStatingBounds;
-    private Rect           floatingItemBounds;
-
-
-    private float floatingItemAlpha = 0.5f;
-    private int   floatingItemBgColor = 0;
-
-    private int viewHandleId = -1;
-
-    private List<Integer> excludedPositions = new ArrayList<Integer>();
-
-    OnItemMovedListener moveInterface;
-
-    private boolean isDragging;
-    @Nullable
-    OnDragStateChangedListener dragStateChangedListener;
-
-
-
-    public interface OnItemMovedListener
-    {
-        public void onItemMoved(int from, int to);
-    }
-
-    public interface OnDragStateChangedListener {
-        public void onDragStart();
-        public void onDragStop();
-    }
-
-    private void debugLog(String log)
-    {
-        if (DEBUG)
-            Log.d(TAG, log);
-    }
-
-    public RecyclerView.OnScrollListener getScrollListener()
-    {
-        return scrollListener;
-    }
-
-    /*
-     * Set the item move interface
-     */
-    public void setOnItemMovedListener(OnItemMovedListener swif)
-    {
-        moveInterface = swif;
-    }
-
-    public void setViewHandleId(int id)
-    {
-        viewHandleId = id;
-    }
-
-    public void setLeftDragArea(int w)
-    {
-        dragHandleWidth = w;
-    }
-
-    public void setFloatingAlpha(float a)
-    {
-        floatingItemAlpha = a;
-    }
-
-    public void setFloatingBgColor(int c)
-    {
-        floatingItemBgColor = c;
-    }
-    /*
-     Set the window at top and bottom of list, must be between 0 and 0.5
-     For example 0.1 uses the top and bottom 10% of the lists for scrolling
-     */
-    public void setAutoScrollWindow(float w)
-    {
-        autoScrollWindow = w;
-    }
-
-    /*
-    Set the autoscroll speed, default is 0.5
-     */
-    public void setAutoScrollSpeed(float speed)
-    {
-        autoScrollSpeed = speed;
-    }
-
-    @Override
-    public void getItemOffsets(Rect outRect, View view, RecyclerView rv, RecyclerView.State state) {
-        super.getItemOffsets(outRect, view, rv, state);
-
-        debugLog("getItemOffsets");
-
-        debugLog("View top = " + view.getTop());
-        if (selectedDragItemPos != -1)
-        {
-            int itemPos =  rv.getChildPosition(view);
-            debugLog("itemPos =" + itemPos);
-            
-            if(!canDragOver(itemPos)) {
-                return;
-            }
-
-            //Movement of finger
-            float totalMovement = fingerY-fingerAnchorY;
-
-            if (itemPos == selectedDragItemPos)
-            {
-                view.setVisibility(View.INVISIBLE);
-            }
-            else
-            {
-                //Make view visible incase invisible
-                view.setVisibility(View.VISIBLE);
-
-                //Find middle of the floatingItem
-                float floatMiddleY = floatingItemBounds.top + floatingItemBounds.height()/2;
-
-                //Moving down the list
-                //These will auto-animate if the device continually sends touch motion events
-               // if (totalMovment>0)
-                {
-                    if ((itemPos > selectedDragItemPos) && (view.getTop() < floatMiddleY))
-                    {
-                        float amountUp = (floatMiddleY - view.getTop()) / (float)view.getHeight();
-                      //  amountUp *= 0.5f;
-                        if (amountUp > 1)
-                            amountUp = 1;
-
-                        outRect.top = -(int)(floatingItemBounds.height()*amountUp);
-                        outRect.bottom = (int)(floatingItemBounds.height()*amountUp);
-                    }
-
-                }//Moving up the list
-               // else if (totalMovment < 0)
-                {
-                    if((itemPos < selectedDragItemPos) && (view.getBottom() > floatMiddleY))
-                    {
-                        float amountDown = ((float)view.getBottom() - floatMiddleY) / (float)view.getHeight();
-                      //  amountDown *= 0.5f;
-                        if (amountDown > 1)
-                            amountDown = 1;
-
-                        outRect.top = (int)(floatingItemBounds.height()*amountDown);
-                        outRect.bottom = -(int)(floatingItemBounds.height()*amountDown);
-                    }
-                }
-            }
-        }
-        else
-        {
-            outRect.top = 0;
-            outRect.bottom = 0;
-            //Make view visible incase invisible
-            view.setVisibility(View.VISIBLE);
-        }
-    }
-
-    /**
-     * Find the new position by scanning through the items on
-     * screen and finding the positional relationship.
-     * This *seems* to work, another method would be to use
-     * getItemOffsets, but I think that could miss items?..
-     */
-    private int getNewPostion(RecyclerView rv)
-    {
-        int itemsOnScreen = rv.getLayoutManager().getChildCount();
-
-        float floatMiddleY = floatingItemBounds.top + floatingItemBounds.height()/2;
-
-        int above=0;
-        int below = Integer.MAX_VALUE;
-        for (int n=0;n < itemsOnScreen;n++) //Scan though items on screen, however they may not
-        {                                   // be in order!
-
-            View view = rv.getLayoutManager().getChildAt(n);
-
-            if (view.getVisibility() != View.VISIBLE)
-                continue;
-
-            int itemPos = rv.getChildPosition(view);
-
-            if (itemPos == selectedDragItemPos) //Don't check against itself!
-                continue;
-
-            float viewMiddleY = view.getTop() + view.getHeight()/2;
-            if (floatMiddleY > viewMiddleY) //Is above this item
-            {
-                if (itemPos > above)
-                    above = itemPos;
-            }
-            else if (floatMiddleY <= viewMiddleY) //Is below this item
-            {
-                if (itemPos < below)
-                    below = itemPos;
-            }
-        }
-        debugLog("above = " + above + " below = " + below);
-
-        if (below != Integer.MAX_VALUE) {
-            if (below < selectedDragItemPos) //Need to count itself
-                below++;
-            return below - 1;
-        }
-        else
-        {
-            if (above < selectedDragItemPos)
-                above++;
-
-            return above;
-        }
-    }
-
-
-    @Override
-    public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
-       debugLog("onInterceptTouchEvent");
-
-        if (e.getAction() == MotionEvent.ACTION_MOVE && isDragging) {
-            return true;
-        }
-        if ((e.getAction() == MotionEvent.ACTION_UP || e.getAction() == MotionEvent.ACTION_CANCEL) &&
-                isDragging) {
-            setIsDragging(false);
-            selectedDragItemPos = -1;
-            floatingItem = null;
-            rv.invalidateItemDecorations();
-            return false;
-        }
-        if (e.getAction() == MotionEvent.ACTION_DOWN)
-        {
-            View itemView = rv.findChildViewUnder(e.getX(), e.getY());
-
-            if (itemView==null)
-                return false;
-
-            boolean dragging = false;
-
-            if ((dragHandleWidth > 0 ) && (e.getX() < dragHandleWidth))
-            {
-                dragging = true;
-            }
-            else if (viewHandleId != -1)
-            {
-                //Find the handle in the list item
-                View handleView = itemView.findViewById(viewHandleId);
-
-                if (handleView == null)
-                {
-                    Log.e(TAG, "The view ID " + viewHandleId + " was not found in the RecycleView item");
-                    return false;
-                }
-
-                //View should be visible to drag
-                if(handleView.getVisibility()!=View.VISIBLE) {
-                    return false;
-                }
-
-                //We need to find the relative position of the handle to the parent view
-                //Then we can work out if the touch is within the handle
-                int[] parentItemPos = new int[2];
-                itemView.getLocationInWindow(parentItemPos);
-
-                int[] handlePos = new int[2];
-                handleView.getLocationInWindow(handlePos);
-
-                int xRel = handlePos[0] - parentItemPos[0];
-                int yRel = handlePos[1] - parentItemPos[1];
-
-                Rect touchBounds = new Rect(itemView.getLeft() + xRel, itemView.getTop() + yRel,
-                        itemView.getLeft() + xRel + handleView.getWidth(),
-                        itemView.getTop() + yRel  + handleView.getHeight()
-                );
-
-                if (touchBounds.contains((int)e.getX(), (int)e.getY()))
-                    dragging = true;
-
-                debugLog("parentItemPos = " + parentItemPos[0] + " " + parentItemPos[1]);
-                debugLog("handlePos = " + handlePos[0] + " " + handlePos[1]);
-            }
-
-
-            if (dragging)
-            {
-                debugLog("Started Drag");
-
-                setIsDragging(true);
-
-                floatingItem = createFloatingBitmap(itemView);
-
-                fingerAnchorY = (int)e.getY();
-                fingerOffsetInViewY = fingerAnchorY - itemView.getTop();
-                fingerY = fingerAnchorY;
-
-                selectedDragItemPos = rv.getChildPosition(itemView);
-                debugLog("selectedDragItemPos = " + selectedDragItemPos);
-
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public void onTouchEvent(RecyclerView rv, MotionEvent e) {
-        debugLog("onTouchEvent");
-
-        if ((e.getAction() == MotionEvent.ACTION_UP) ||
-                (e.getAction() == MotionEvent.ACTION_CANCEL))
-        {
-            if ((e.getAction() == MotionEvent.ACTION_UP) && selectedDragItemPos != -1)
-            {
-                int newPos = getNewPostion(rv);
-                if (moveInterface != null) {
-                    if (canDragOver(selectedDragItemPos) && canDragOver(newPos))
-                        moveInterface.onItemMoved(selectedDragItemPos, newPos);
-                }
-            }
-
-            setIsDragging(false);
-            selectedDragItemPos = -1;
-            floatingItem = null;
-            rv.invalidateItemDecorations();
-            return;
-        }
-
-
-        fingerY = (int)e.getY();
-
-        if (floatingItem!=null)
-        {
-            floatingItemBounds.top = fingerY - fingerOffsetInViewY;
-
-            if (floatingItemBounds.top < -floatingItemStatingBounds.height()/2) //Allow half the view out the top
-                floatingItemBounds.top = -floatingItemStatingBounds.height()/2;
-
-            floatingItemBounds.bottom = floatingItemBounds.top + floatingItemStatingBounds.height();
-
-            floatingItem.setBounds(floatingItemBounds);
-        }
-
-        //Do auto scrolling at end of list
-        float scrollAmount=0;
-        if (fingerY > (rv.getHeight() * (1-autoScrollWindow)))
-        {
-            scrollAmount = (fingerY - (rv.getHeight() * (1-autoScrollWindow)));
-        }
-        else if (fingerY < (rv.getHeight() * autoScrollWindow))
-        {
-            scrollAmount = (fingerY - (rv.getHeight() * autoScrollWindow));
-        }
-        debugLog("Scroll: " + scrollAmount);
-
-        scrollAmount *= autoScrollSpeed;
-        rv.scrollBy(0, (int)scrollAmount);
-
-        rv.invalidateItemDecorations();// Redraw
-    }
-
-    @Override
-    public void onRequestDisallowInterceptTouchEvent(boolean b) {
-        // Do nothing
-    }
-
-    private void setIsDragging(final boolean dragging) {
-        if(dragging != isDragging) {
-            isDragging = dragging;
-            if(dragStateChangedListener != null) {
-                if (isDragging) {
-                    dragStateChangedListener.onDragStart();
-                } else {
-                    dragStateChangedListener.onDragStop();
-                }
-            }
-        }
-    }
-
-    public void setOnDragStateChangedListener(final OnDragStateChangedListener dragStateChangedListener) {
-        this.dragStateChangedListener = dragStateChangedListener;
-    }
-
-
-    Paint bgColor = new Paint();
-    @Override
-    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-        if (floatingItem != null) {
-            floatingItem.setAlpha((int)(255 * floatingItemAlpha));
-            bgColor.setColor(floatingItemBgColor);
-            c.drawRect(floatingItemBounds,bgColor);
-            floatingItem.draw(c);
-        }
-    }
-
-    RecyclerView.OnScrollListener scrollListener = new RecyclerView.OnScrollListener() {
-        @Override
-        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-            super.onScrollStateChanged(recyclerView, newState);
-        }
-
-        @Override
-        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-            super.onScrolled(recyclerView, dx, dy);
-            debugLog("Scrolled: " + dx + " " + dy);
-            fingerAnchorY -= dy;
-        }
-    };
-    
-    /**
-     *
-     *
-     * @param position
-     * @return True if we can drag the item over this position, False if not.
-     */
-    protected boolean canDragOver(int position) {
-        return !excludedPositions.contains(position);
-    }
-
-
-    public void addExcludedDraggingPosition(int position) {
-        excludedPositions.add(position);
-    }
-
-    private BitmapDrawable createFloatingBitmap(View v)
-    {
-        floatingItemStatingBounds = new Rect(v.getLeft(), v.getTop(),v.getRight(), v.getBottom());
-        floatingItemBounds = new Rect(floatingItemStatingBounds);
-
-        Bitmap bitmap = Bitmap.createBitmap(floatingItemStatingBounds.width(),
-                floatingItemStatingBounds.height(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        v.draw(canvas);
-
-        BitmapDrawable retDrawable = new BitmapDrawable(v.getResources(), bitmap);
-        retDrawable.setBounds(floatingItemBounds);
-
-        return retDrawable;
-    }
-}

app/src/main/java/org/puder/trs80/MainActivity.java
@@ -32,20 +32,21 @@ import android.support.v7.app.MediaRouteButton;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.emtronics.dragsortrecycler.DragSortRecycler;
-
 import org.puder.trs80.cast.CastMessageSender;
 import org.puder.trs80.cast.RemoteCastScreen;
+import org.puder.trs80.drag.OnStartDragListener;
+import org.puder.trs80.drag.ConfigurationItemTouchHelperCallback;
 
 import java.io.File;
 
 public class MainActivity extends BaseActivity implements
         InitialSetupDialogFragment.DownloadCompletionListener, ConfigurationMenuListener,
-        PopupMenu.OnMenuItemClickListener, PopupMenu.OnDismissListener {
+        PopupMenu.OnMenuItemClickListener, PopupMenu.OnDismissListener, OnStartDragListener {
 
     private static final int     REQUEST_CODE_EDIT_CONFIG   = 1;
     private static final int     REQUEST_CODE_RUN_EMULATOR  = 2;
@@ -65,7 +66,7 @@ public class MainActivity extends BaseActivity implements
     private static final int     MENU_OPTION_DELETE         = 4;
 
     private RecyclerView         configurationListView;
-    private RecyclerView.Adapter configurationListViewAdapter;
+    private ConfigurationListViewAdapter configurationListViewAdapter;
     private Configuration        currentConfiguration;
     private int                  currentConfigurationPosition;
     private SharedPreferences    sharedPrefs;
@@ -74,6 +75,8 @@ public class MainActivity extends BaseActivity implements
 
     private CastMessageSender    castMessageSender;
 
+
+    private ItemTouchHelper mItemTouchHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -84,32 +87,14 @@ public class MainActivity extends BaseActivity implements
         this.setContentView(R.layout.main_activity);
 
         castMessageSender = CastMessageSender.get();
-        configurationListViewAdapter = new ConfigurationListViewAdapter(this);
+        configurationListViewAdapter = new ConfigurationListViewAdapter(this, this);
         configurationListView = (RecyclerView) this.findViewById(R.id.list_configurations);
         configurationListView.setLayoutManager(new LinearLayoutManager(this));
         configurationListView.setAdapter(configurationListViewAdapter);
 
-        DragSortRecycler dragSortRecycler = new DragSortRecycler();
-        dragSortRecycler.setViewHandleId(R.id.configuration_reorder);
-        dragSortRecycler.setFloatingAlpha(0.4f);
-        dragSortRecycler.setFloatingBgColor(0x80FFFFFF);
-        dragSortRecycler.setAutoScrollSpeed(0.3f);
-        dragSortRecycler.setAutoScrollWindow(0.1f);
-        dragSortRecycler.addExcludedDraggingPosition(0);
-        dragSortRecycler.setOnItemMovedListener(new DragSortRecycler.OnItemMovedListener() {
-            @Override
-            public void onItemMoved(int from, int to) {
-                if (from == to) {
-                    return;
-                }
-                Configuration.move(from - 1, to - 1);
-                configurationListViewAdapter.notifyDataSetChanged();
-            }
-        });
-
-        configurationListView.addItemDecoration(dragSortRecycler);
-        configurationListView.addOnItemTouchListener(dragSortRecycler);
-        configurationListView.setOnScrollListener(dragSortRecycler.getScrollListener());
+        ItemTouchHelper.Callback callback = new ConfigurationItemTouchHelperCallback(configurationListViewAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(configurationListView);
     }
 
     @Override
@@ -521,5 +506,10 @@ public class MainActivity extends BaseActivity implements
         }
         updateView(-1, -1, -1);
         AlertDialogUtil.showHint(MainActivity.this, R.string.hint_configuration_usage);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }

commit 0a14858b37441b217221cbe5dd41f8706e5c7c3e

Fix for bug introduced in dedb8d2

app/src/main/java/org/puder/trs80/ConfigurationListViewAdapter.java
@@ -39,7 +39,7 @@ public class ConfigurationListViewAdapter extends
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
-        Configuration.move(fromPosition - 1, fromPosition - 1);
+        Configuration.move(fromPosition - 1, toPosition - 1);
         notifyItemMoved(fromPosition, toPosition);
         return true;
     }